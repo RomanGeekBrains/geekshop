a5d9a28f262a651ebc94b91194226e46edcd3d3f
RomanGeekBrains <Roman_Lebedev91@mail.ru>
2022-01-13 01:56:51 +0600

    url revision

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,20 +1,34 @@
-from django.conf import settings
-from django.conf.urls.static import static
+# from django.conf import settings
+# from django.conf.urls.static import static
+# from django.urls import path
+
+# from .views import *
+
+# urlpatterns = [
+#     path("", main, name="home"),
+#     path("products/", products, name="products"),
+#     path("products/all", products, name="products_all"),
+#     path("products/home", products, name="products_home"),
+#     path("products/office", products, name="products_office"),
+#     path("products/modern", products, name="products_modern"),
+#     path("products/classic", products, name="products_classic"),
+#     path("contact/", contact, name="contact"),
+# ]
+
+
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
+
 from django.urls import path
 
-from .views import *
+import mainapp.views as mainapp
+
+app_name = "mainapp"
 
 urlpatterns = [
-    path("", main, name="home"),
-    path("products/", products, name="products"),
-    path("products/all", products, name="products_all"),
-    path("products/home", products, name="products_home"),
-    path("products/office", products, name="products_office"),
-    path("products/modern", products, name="products_modern"),
-    path("products/classic", products, name="products_classic"),
-    path("contact/", contact, name="contact"),
+    path("", mainapp.products, name="index"),
+    path("<int:pk>/", mainapp.products, name="category"),
 ]
-
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

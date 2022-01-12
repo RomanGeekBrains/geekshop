71c7610a3e60194a84317a67635257b12ff905ae
RomanGeekBrains <83689860+RomanGeekBrains@users.noreply.github.com>
2022-01-12 14:15:56 -0500

    Lesson 3 (#4)
    * Lesson 3. Django ORM start
    
    * Lesson 3. Djagno admin
    
    * Lesson 3. Urls for app
    
    * Lesson 3. Code style fix

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,8 +1,8 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
 from .views import *
-
-
 
 urlpatterns = [
     path("", main, name="home"),
@@ -14,3 +14,7 @@ urlpatterns = [
     path("products/classic", products, name="products_classic"),
     path("contact/", contact, name="contact"),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,38 +1,40 @@
 import json
+
+from django.conf import settings
 from django.shortcuts import render
+
+from .models import Product, ProductCategory
 
 
 def main(request):
     title = "домашняя"
-    with open('mainapp/data_json/products.json', 'r') as file_with_products:
-        products = json.loads(file_with_products.read())['products']
 
-    content = {"title": title, "products": products}
+    products = Product.objects.all()
+
+    content = {"title": title, "products": products, "media_url": settings.MEDIA_URL}
     return render(request, "mainapp/index.html", content)
 
 
 def products(request):
     title = "продукты"
-    links_menu = [
-        {"href": "products_all", "name": "все"},
-        {"href": "products_home", "name": "дом"},
-        {"href": "products_office", "name": "офис"},
-        {"href": "products_modern", "name": "модерн"},
-        {"href": "products_classic", "name": "классика"},
-    ]
 
-    with open('mainapp/data_json/same_products.json', 'r') as file_with_same_products:
-        same_products = json.loads(file_with_same_products.read())['same_products']
+    links_menu = ProductCategory.objects.all()
+    same_products = Product.objects.all()
+    content = {
+        "title": title,
+        "links_menu": links_menu,
+        "same_products": same_products,
+        "media_url": settings.MEDIA_URL,
+    }
 
-    content = {"title": title, "links_menu": links_menu, "same_products": same_products}
     return render(request, "mainapp/products.html", content)
 
 
 def contact(request):
     title = "о нас"
-    
-    with open('mainapp/data_json/contact_info.json', 'r') as file_with_contact_info:
-        locations = json.loads(file_with_contact_info.read())['locations']
 
-    content = {"title": title,  "locations": locations}
+    with open("mainapp/data_json/contact_info.json", "r") as file_with_contact_info:
+        locations = json.loads(file_with_contact_info.read())["locations"]
+
+    content = {"title": title, "locations": locations}
     return render(request, "mainapp/contact.html", content)

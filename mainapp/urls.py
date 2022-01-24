ba3cd928b2266a2a222dab5f4c1c251b935c6229
RomanGeekBrains <Roman_Lebedev91@mail.ru>
2022-01-24 20:27:15 +0600

    Lesson 6. Product page

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,25 +1,3 @@
-# from django.conf import settings
-# from django.conf.urls.static import static
-# from django.urls import path
-
-# from .views import *
-
-# urlpatterns = [
-#     path("", main, name="home"),
-#     path("products/", products, name="products"),
-#     path("products/all", products, name="products_all"),
-#     path("products/home", products, name="products_home"),
-#     path("products/office", products, name="products_office"),
-#     path("products/modern", products, name="products_modern"),
-#     path("products/classic", products, name="products_classic"),
-#     path("contact/", contact, name="contact"),
-# ]
-
-
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
 from django.urls import path
 
 import mainapp.views as mainapp
@@ -29,4 +7,5 @@ app_name = "mainapp"
 urlpatterns = [
     path("", mainapp.products, name="index"),
     path("category/<int:pk>/", mainapp.products, name="category"),
+    path("product/<int:pk>/", mainapp.product, name="product"),
 ]

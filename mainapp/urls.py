1edf1d5d6804d7845cc096ad1abed704b6cd244b
RomanGeekBrains <83689860+RomanGeekBrains@users.noreply.github.com>
2022-02-16 00:39:44 -0500

    Lesson 8

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -2,10 +2,16 @@ from django.urls import path
 
 import mainapp.views as mainapp
 
-app_name = "mainapp"
+from .apps import MainappConfig
+
+# from mainapp.views import *
+
+
+app_name = MainappConfig.name
 
 urlpatterns = [
     path("", mainapp.products, name="index"),
     path("category/<int:pk>/", mainapp.products, name="category"),
+    path("category/<int:pk>/page/<int:page>/", mainapp.products, name="page"),
     path("product/<int:pk>/", mainapp.product, name="product"),
 ]
